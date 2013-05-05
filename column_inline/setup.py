7e1765f46dc076edc20a49ea79996d2f695547d7
andybak <andy@andybak.net>
2013-05-05 10:12:55 +0100

    make setup.py installable

---
+++ column_inline/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from setuptools import setup
+from setuptools import find_packages
+
+long_desc = open('README.md').read()
+
+setup(
+    name='django-columninline',
+    version='0.1',
+    description='Django ColumnInline',
+    packages=find_packages(),
+    long_description=long_desc,
+    url='https://github.com/DjangoAdminHackers/ColumnInline',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
