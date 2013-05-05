17768c1117edc417be751d6b9799120167e97316
andybak <andy@andybak.net>
2013-05-05 16:53:18 +0100

    Fix a few style issues

--- column_inline/admin.py
+++ column_inline/admin.py
@@ -3,4 +3,8 @@ from django.contrib.admin.options import InlineModelAdmin
 # Custom Column Inline model admin 
 class ColumnInline(InlineModelAdmin):
     template = 'admin/column_stacked.html'
+    class Media:
+            css = {
+                "all": ("column_inline.css",)
+            }
 
