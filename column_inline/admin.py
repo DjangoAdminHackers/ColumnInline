0c496f9e14f52c5a56d5410932ccb29d0555a478
Andy Baker <andy@andybak.net>
2016-10-07 09:59:07 +0100

    add compact inlines

--- column_inline/admin.py
+++ column_inline/admin.py
@@ -1,10 +1,17 @@
 from django.contrib.admin.options import InlineModelAdmin
 
-# Custom Column Inline model admin 
+ 
 class ColumnInline(InlineModelAdmin):
-    template = 'admin/column_stacked.html'
+    
+    template = 'admin/edit_inline/column_stacked.html'
+    
     class Media:
-            css = {
-                "all": ("column_inline.css",)
-            }
+        css = {"all": ("column_inline.css",)}
 
+
+class CompactInline(InlineModelAdmin):
+    
+    template = 'admin/edit_inline/compact_stacked.html'
+    
+    class Media:
+        css = {"all": ("compact_inline.css",)}
